d6ed7961a2341a647643271f55b897c63591946d
prabhat.kumar.verma <prabhatkmr626@gmail.com>
2023-02-05 01:18:15 +0530

    getAllUser,getUser,DeleteUser,UpdateUser API

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -4,7 +4,7 @@ const { generateToken } = require('../configs/jwttoken');
 
 const createUser = asyncHandler(async (req, res) => {
     const email = req.body.email;
-    const findUser = await User.findOne({email:email});
+    const findUser = await User.findOne({ email: email });
     if (!findUser) {
         const newUser = await User.create(req.body);
         res.json(newUser);
@@ -15,16 +15,16 @@ const createUser = asyncHandler(async (req, res) => {
 });
 
 const loginUserCtrl = asyncHandler(async (req, res) => {
-    const {email, password} = req.body;
-    const findUser = await User.findOne({email});
-    if(findUser && await findUser.isPasswordMatched(password)) {
+    const { email, password } = req.body;
+    const findUser = await User.findOne({ email });
+    if (findUser && await findUser.isPasswordMatched(password)) {
         res.json({
-            _id:findUser._id,
-            firstname:findUser.firstname,
-            lastname:findUser?.lastname,
-            email:findUser?.email,
-            mobile:findUser?.mobile,
-            role:findUser?.role,
+            _id: findUser._id,
+            firstname: findUser.firstname,
+            lastname: findUser?.lastname,
+            email: findUser?.email,
+            mobile: findUser?.mobile,
+            role: findUser?.role,
             token: await generateToken(findUser?._id)
         });
     } else {
@@ -32,4 +32,50 @@ const loginUserCtrl = asyncHandler(async (req, res) => {
     }
 });
 
-module.exports = {createUser,loginUserCtrl};
+const getallUser = asyncHandler(async (req, res) => {
+    try {
+        const getallUser = await User.find();
+        res.json(getallUser);
+    } catch (ex) {
+        throw new Error(ex)
+    }
+});
+
+const getaUser = asyncHandler(async (req, res) => {
+    try {
+        const {id} = req.params;
+        const getUser = await User.findById(id);
+        res.json(getUser);
+    } catch (ex) {
+        throw new Error(ex)
+    }
+});
+
+const deleteaUser = asyncHandler(async (req, res) => {
+    try {
+        const {id} = req.params;
+        const deleteUser = await User.findByIdAndDelete(id);
+        res.json(deleteUser);
+    } catch (ex) {
+        throw new Error(ex)
+    }
+});
+
+const updateaUser = asyncHandler(async (req, res) => {
+    try {
+        const {id} = req.params;
+        const deleteUser = await User.findByIdAndUpdate(id,{
+            $set:{
+                firstname:req?.body?.firstname,
+                lastname:req?.body?.lastname,
+                email:req?.body?.email,
+                mobile:req?.body?.mobile,
+            }
+        },{ returnOriginal: false });
+        res.json(deleteUser);
+    } catch (ex) {
+        throw new Error(ex)
+    }
+});
+
+module.exports = { createUser, loginUserCtrl, getallUser,getaUser,deleteaUser,updateaUser };
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,7 +1,16 @@
 const express = require('express');
-const { createUser,loginUserCtrl } = require('../controller/userCtrl');
+const { createUser,
+    loginUserCtrl,
+    getallUser, 
+    getaUser, 
+    deleteaUser,
+    updateaUser} = require('../controller/userCtrl');
 const router = express.Router();
 
 router.post('/register',createUser);
 router.post('/login',loginUserCtrl);
+router.get('/all-Users',getallUser);
+router.get('/:id',getaUser);
+router.delete('/:id',deleteaUser);
+router.put('/:id',updateaUser);
 module.exports = router;
