19631e8f295c822f5b2b85430ca07e2cd8d96322
prabhat.kumar.verma <prabhatkmr626@gmail.com>
2023-02-05 04:45:50 +0530

    refresh token added

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -7,7 +7,8 @@ const {
     deleteaUser,
     updateaUser,
     blockUser,
-    unblockUser
+    unblockUser,
+    handleRefreshToken
 } = require('../controller/userCtrl');
 const { authMiddleware, isAdmin } = require('../middlewares/authMiddleware');
 const router = express.Router();
@@ -15,9 +16,11 @@ const router = express.Router();
 router.post('/register',createUser);
 router.post('/login',loginUserCtrl);
 router.get('/all-Users',getallUser);
+router.get('/refresh', handleRefreshToken);
 router.get('/:id', authMiddleware, isAdmin, getaUser);
 router.delete('/:id',deleteaUser);
 router.put('/edit-user', authMiddleware, updateaUser);
 router.put('/block-user/:id', authMiddleware,isAdmin, blockUser);
 router.put('/unblock-user/:id', authMiddleware,isAdmin, unblockUser);
+
 module.exports = router;
