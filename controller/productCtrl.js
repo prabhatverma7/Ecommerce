555b9fc1439f302a7b88ee6d5a3088933ed0dc7d
prabhat.kumar.verma <prabhatkmr626@gmail.com>
2023-02-06 03:31:45 +0530

    Apply filter, Sort, paghination

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -24,7 +24,7 @@ const updateProduct = asyncHandler(async (req, res) => {
         if (req.body.title) {
             req.body.slug = slugify(req.body.title);
         }
-        const updateProduct = await Product.findOneAndUpdate({id}, req.body, { new: true });
+        const updateProduct = await Product.findOneAndUpdate({ id }, req.body, { new: true });
         res.json(updateProduct);
     } catch (error) {
         throw new Error(error);
@@ -36,12 +36,12 @@ const deleteProduct = asyncHandler(async (req, res) => {
     const id = req.params;
     validateMongoDbId(id);
     try {
-      const deleteProduct = await Product.findOneAndDelete(id);
-      res.json(deleteProduct);
+        const deleteProduct = await Product.findOneAndDelete(id);
+        res.json(deleteProduct);
     } catch (error) {
-      throw new Error(error);
+        throw new Error(error);
     }
-  });
+});
 
 const getaProduct = asyncHandler(async (req, res) => {
     const { id } = req.params;
@@ -54,11 +54,50 @@ const getaProduct = asyncHandler(async (req, res) => {
     }
 });
 
+// Get All Products
 const getAllProduct = asyncHandler(async (req, res) => {
 
     try {
-        const getAllProduct = await Product.find();
-        res.json(getAllProduct);
+        // Filtering product
+        const queryObj = { ...req.query };
+        const excludeFields = ["page", "sort", "limit", "fields"];
+        excludeFields.forEach((el) => delete queryObj[el]);
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+        let query = Product.find(JSON.parse(queryStr));
+
+        // Sorting product
+
+        if (req.query.sort) {
+            const sortBy = req.query.sort.split(",").join(" ");
+            query = query.sort(sortBy);
+        } else {
+            query = query.sort("-createdAt");
+        }
+
+        // limiting the fields
+
+        if (req.query.fields) {
+            const fields = req.query.fields.split(",").join(" ");
+            query = query.select(fields);
+        } else {
+            query = query.select("-__v");
+        }
+
+        // pagination 
+
+        const page = req.query.page;
+        const limit = req.query.limit;
+        const skip = (page - 1) * limit;
+        query = query.skip(skip).limit(limit);
+        if (req.query.page) {
+            const productCount = await Product.countDocuments();
+            if (skip >= productCount) throw new Error("This Page does not exists");
+        }
+        const product = await query;
+        res.json(product);
+
     } catch (error) {
         throw new Error(error);
     }
